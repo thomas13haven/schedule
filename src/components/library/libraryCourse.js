95ab656787507578523574fe568539025168ec93
Jordan Hudgens <jordan@devcamp.com>
2018-07-18 13:31:43 -0600

    Added good icons and used flexbox to make it in yth right place

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Icon from '../icon' 
+import Icon from "../icon";
+import Arrow from "../arrow";
 
 class LibraryCourse extends Component {
   render() {
     return (
       <div className="library-course">
-        <label className="library-course__title">Problem solving</label>
-       { Icon('fas fa-check', 'library-course__icon') }
-        {/* arrow component*/}
-        {/* action button component*/} 
+        <div className="library-course__title-arrow">
+          <label className="library-course__title">Problem solving</label>
+          {Icon("fas fa-check", "library-course__icon")}
+        </div>
+        <Arrow className="library-course__arrow" />
+        {/* action button component*/}
         <div className="library-course__description">
-            <label>Course Description</label>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices hendrerit urna, ac pellentesque nunc pharetra id. Donec scelerisque sapien est, ac gravida mauris eleifend at.</p>
-        </div>     
+          <label>Course Description</label>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            ultrices hendrerit urna, ac pellentesque nunc pharetra id. Donec
+            scelerisque sapien est, ac gravida mauris eleifend at.
+          </p>
+        </div>
       </div>
     );
   }
