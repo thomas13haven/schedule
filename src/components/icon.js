7d85718616ab490843b68e716cf8e8556b9d2273
Jordan Hudgens <jordan@devcamp.com>
2018-07-18 13:14:30 -0600

    added an  icon with reusable code

---
+++ src/components/icon.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Icon = (icon, className) => {
+    return (
+      <i className={`${icon} ${className}`}></i>
+ 
+    );  
+}
+
+export default Icon;
