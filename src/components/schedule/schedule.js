730ad7bb70e892f57629e2ec04c8de1cc9f9537d
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 08:06:51 -0600

    Adedd loades of styles

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Gradient from '../gradient';
 import ScheduleCourse from './scheduleCourse';
+import ProgressTracker from './progressTracker';
 
  class Schedule extends Component {
   render() {
@@ -9,7 +10,8 @@ import ScheduleCourse from './scheduleCourse';
             <h1 className="schedule__title">My Schedule</h1>
             <ScheduleCourse title={'Problem Solving'}/>
             <ScheduleCourse title={'Problem Solving'}/>
-            <ScheduleCourse title={'Problem Solving'}/>                        
+            <ScheduleCourse title={'Problem Solving'}/>  
+            <ProgressTracker/>                      
             <Gradient/>
       </div>
     );
