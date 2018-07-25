21805cb6d9250d328b1bfaf497b687161f8b19c9
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 08:31:04 -0600

    added basic redux stuff

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import * as actions from '../../actions';
 import LibraryCourse from './libraryCourse';
 
  class Library extends Component {
+
+
+  componentDidMount() {
+    this.props.fetchCourses()
+  }
+
+
   render() {
+
     return (
       <div className="library">
       <h1 className="library__title">Course Library</h1>
@@ -15,4 +25,4 @@ import LibraryCourse from './libraryCourse';
   }
 }
 
-export default Library;
+export default connect(null, actions)(Library);
