e00c1e44f4255754d38c9c9ffbac2e20b63c4c3e
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 12:45:58 -0600

    Rendering the description when arro wis pressed/clicked

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -12,20 +12,21 @@ class Arrow extends Component {
   }
 
   toggleArrow = function() {
-   if(this.state.status) {
-     //closes it
-     document.getElementById('arrow').classList.remove('arrow-closed');     
-   } else {
-     //opens it
-     document.getElementById('arrow').classList.add('arrow-closed');
-   }
+    this.props.callback(this.state.status)
+    if(this.state.status) {
+        document.getElementById(this.id).classList.remove('arrow-closed');
+    } else {
+        document.getElementById(this.id).classList.add('arrow-closed');
+    }
 
-   this.setState({ status: !this.state.status })
-  }.bind(this);
+    this.setState({ status: !this.state.status })
+}.bind(this);
+
 
   render() {
+     this.id = `arrow-${this.props.id}`
         return (
-            <a id="arrow" onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+            <a id={this.id} onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
         )
     }
 }
