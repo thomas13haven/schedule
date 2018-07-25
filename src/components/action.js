4087f8f56db77ce36231c992908c874d9a2d6019
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 13:54:51 -0600

    Added and animation npm package to create the animation of the package opening

--- src/components/action.js
+++ src/components/action.js
@@ -1,14 +1,43 @@
-import React, { Component } from 'react';
-import Icon from './icon';
+import React, { Component } from "react";
+import Icon from "./icon";
 
 class Action extends Component {
+
+
+  constructor(props){
+    super(props)
+
+    this.status = false;
+  }
+
+
+  handleAction = function() {
+    this.props.onClick();
+    if(!this.status) {
+      document.getElementById(this.id).classList.add("action-remove");
+    } else {
+    document.getElementById(this.id).classList.remove("action-remove");      
+    }
+    this.status = !this.status;
+
+
+  }.bind(this);
+
   render() {
+    this.id = `action ${this.props.id}`
     return (
-      <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-        { Icon('fas fa-plus-circle') }
-        {/* fas fa-times-circle */}
-      </button>
+      <a
+        id={this.id}
+        onClick={() => this.handleAction()}
+        className={`${this.props.className} action`}
+      />
     );
+    // return (
+    //   <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+    //     { Icon('fas fa-plus-circle') }
+    //     {/* fas fa-times-circle */}
+    //   </button>
+    // );
   }
 }
 
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -6,50 +6,62 @@ import Icon from "../icon";
 import Arrow from "../arrow";
 import Action from "../action";
 
-class LibraryCourse extends Component {
+import AnimateHeight from "react-animate-height";
 
+class LibraryCourse extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      status: true
-    }
+      status: true,
+      height: 0
+    };
   }
 
-
-  renderDescription = function() {
-    if (!this.state.status) {    
-      return (  
-        <div className="library-course__description">
-          <label>Course Description</label>
-          <p>{this.props.description}</p>
-        </div>
-      )
+  handleCallback = function(status) {
+    let height = this.state.height == 0 ? 80 : 0;
+    if (!status) {
+      document.getElementById(this.id).classList.add("library-course-selected");
+    } else {
+      document
+        .getElementById(this.id)
+        .classList.remove("library-course-selected");
     }
+    this.setState({ 
+      status ,
+      height
+    });
   }.bind(this);
 
-  handleCallback = function(status) {
-    this.setState({ status})
-  }.bind(this)
-
   render() {
+    this.id = `library-course-${this.props.id}`;
     return (
-      <div className="library-course">
+      <div id={this.id} className="library-course">
         <div className="library-course__title-check">
           <label className="library-course__title">{this.props.title}</label>
           {Icon("fas fa-check", "library-course__icon")}
         </div>
         <div className="library-course__line" />
         <Arrow
-          callback={status => this.handleCallback(status) }
+          callback={status => this.handleCallback(status)}
           id={this.props.id}
           className="library-course__arrow"
         />
         <Action
+          id={this.props.id}
           onClick={() => this.props.toggleEnrolled(this.props.id)}
           className="library-course__action"
         />
-        {this.renderDescription()}
+
+        <AnimateHeight
+        duration ={ 300 }
+        height={this.state.height} 
+      >
+          <div className="library-course__description">
+            <label>Course Description</label>
+            <p>{this.props.description}</p>
+          </div>
+        </AnimateHeight>
       </div>
     );
   }
