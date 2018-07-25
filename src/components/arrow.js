7746b02aab97266e460da6e90f6e66d3ea3e7eb3
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 11:20:21 -0600

    Added better and more styles animated the arrow

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,13 +1,33 @@
+
 import React, { Component } from 'react';
 
 class Arrow extends Component {
-  render() {
-    return (
-      <div className={`${this.props.className} arrow`}>
-        
-      </div>
-    );
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      status: false
+    }
   }
+
+  toggleArrow = function() {
+   if(this.state.status) {
+     //closes it
+     document.getElementById('arrow').classList.remove('arrow-closed');     
+   } else {
+     //opens it
+     document.getElementById('arrow').classList.add('arrow-closed');
+   }
+
+   this.setState({ status: !this.state.status })
+  }.bind(this);
+
+  render() {
+        return (
+            <a id="arrow" onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+        )
+    }
 }
 
 export default Arrow;
